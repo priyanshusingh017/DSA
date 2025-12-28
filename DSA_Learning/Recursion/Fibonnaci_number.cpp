5cb6f0a204bca9c58b46d2d65f40c6877f3ad3f2
albertsingh <albert531singh@gmail.com>
2025-12-28 20:31:17 +0530

    Refactor Fibonacci number implementation for improved readability and consistency

--- DSA_Learning/Recursion/Fibonnaci_number.cpp
+++ DSA_Learning/Recursion/Fibonnaci_number.cpp
@@ -1,40 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void fibonnaci_series1(int n){
-    
+void fibonnaci_series1(int n)
+{
+
     int a = 0;
     int b = 1;
-    cout<< a << " " << b << " " ;
-    for(int i=0; i<n; i++){
-        int c = a+b;
+    cout << a << " " << b << " ";
+    for (int i = 2; i < n; i++)
+    {
+        int c = a + b;
         cout << c << " ";
-        swap(a , b);
-        swap(b , c);
+        swap(a, b);
+        swap(b, c);
     }
 }
 
+int fibonnaci_series(int n)
+{
 
-int fibonnaci_series(int n){
+    // base
+    if (n == 0)
+        return 0;
 
-    //base 
-    if (n==0 ) return 0;
+    if (n == 1)
+        return 1;
 
-    if (n==1) return 1;
-
-    return fibonnaci_series(n-1) + fibonnaci_series(n-2);
-
-
+    return fibonnaci_series(n - 1) + fibonnaci_series(n - 2);
 }
 
-int main(){
+int main()
+{
 
-    int n; 
-    cin>>n;
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    if (n <= 0)
+    {
+        cout << "Please enter a positive integer." << endl;
+    }
+    else
+    {
+        cout << "Fibonacci sequence (iterative): ";
+        fibonnaci_series1(n);
+        cout << endl;
 
-    fibonnaci_series1(n);
-    cout<<endl;
-    cout<<fibonnaci_series(n);
-
+        cout << "Fibonacci sequence (recursive): ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << fibonnaci_series(i) << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
