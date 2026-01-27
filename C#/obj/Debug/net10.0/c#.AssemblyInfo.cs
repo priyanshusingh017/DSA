e7e0dc1e304abd19f2d76b6b22f016ce45593d3a
albertsingh <albert531singh@gmail.com>
2026-01-27 12:18:49 +0530

    Add NuGet configuration and project assets for Second_Largest project
    - Created Second_Largest.csproj.nuget.g.props to define NuGet properties.
    - Added Second_Largest.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

--- C#/obj/Debug/net10.0/c#.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0d6e8f8cc0cb27621b38fcba0c527a57f38bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae36859b08d24aa33dd8cbbe5dd2871a7a82664")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
